739bbe2816a497081ff56ab9f96a25b2b62393d6
Anastasia <nastyajane@yandex.ru>
2022-09-06 23:59:06 +0300

    page project is added

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 import colors from 'vuetify/es5/util/colors'
+// import "vuetify/dist/vuetify.min.css";
 // import '@mdi/font/css/materialdesignicons.css'
 // import { library } from '@fortawesome/fontawesome-svg-core'
 // import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
@@ -58,7 +59,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    '@nuxtjs/auth',
+    // '@nuxtjs/auth',
     '@nuxtjs/moment'
   ],
 
@@ -68,56 +69,58 @@ export default {
   },
 
 
-  auth: {
-  redirect: {
-     login: '/',
-     logout: '/login',
-     callback: '/login',
-     home: '/'
-   },
-   strategies: {
-     local: {
-       token: {
-         property: 'access_token',
-         global: true,
-         // type: 'Bearer'
-     },
-       endpoints: {
-         login: { url: 'http://127.0.0.1:8000/auth/login', method: 'post', propertyName: 'access_token' },
-         user: { url:  'http://127.0.0.1:8000/auth/user', method: 'get', propertyName: 'user' },
-         logout: { url: 'http://127.0.0.1:8000/auth/logout' , method: 'delete'  }
-       },
-       tokenType: ''
-     }
-   }
-  },
+  // auth: {
+  // redirect: {
+  //    login: '/',
+  //    logout: '/login',
+  //    callback: '/login',
+  //    home: '/'
+  //  },
+  //  strategies: {
+  //    local: {
+  //      token: {
+  //        property: 'access_token',
+  //        global: true,
+  //        // type: 'Bearer'
+  //    },
+  //      endpoints: {
+  //        login: { url: 'http://127.0.0.1:8000/auth/login', method: 'post', propertyName: 'access_token' },
+  //        user: { url:  'http://127.0.0.1:8000/auth/user', method: 'get', propertyName: 'user' },
+  //        logout: { url: 'http://127.0.0.1:8000/auth/logout' , method: 'delete'  }
+  //      },
+  //      tokenType: ''
+  //    }
+  //  }
+  // },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    proxy: true,
-    baseURL: 'http://127.0.0.1:8000/auth',
-    browserBaseURL:  'http://127.0.0.1:8001/api',
-   // Your other configurations
-  },
-
-  proxy: {
-    '/auth/': { target: 'http://127.0.0.2:8000', pathRewrite: {'^/auth/': ''} }
-  },
+  // axios: {
+  //   proxy: true,
+  //   baseURL: 'http://127.0.0.1:8000/auth',
+  //   browserBaseURL:  'http://127.0.0.1:8001/api',
+  //  // Your other configurations
+  // },
+  //
+  // proxy: {
+  //   '/auth/': { target: 'http://127.0.0.2:8000', pathRewrite: {'^/auth/': ''} }
+  // },
 
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/styles/scss/variables.scss', '~/assets/styles/scss/selectors.scss'],
     theme: {
-      dark: false,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
+        light: {
+          primary: '#3914AF',
+          accent: '#1E0A5C',
+          secondary: '#F5F5FF',
+          info: '#1E0A5C',
+          warning: '#3914AF',
           error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+          success: colors.green.accent3,
+          purplee: "#351BA9",
+          bluedeep: "#1E0A5C",
+          viewColor: '#C4B9E7'
         }
       }
     }
